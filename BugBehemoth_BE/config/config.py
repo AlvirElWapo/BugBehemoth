4410cd9a4eaa9f5afa70ba313b0dbb3fd231c6ef
LuisMtz-uaemex <84350730+LuisMtz-uaemex@users.noreply.github.com>
2024-08-27 18:46:20 -0600

    ejemplo de endpoint

--- BugBehemoth_BE/config/config.py
+++ BugBehemoth_BE/config/config.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'mi_clave_secreta'
-    DEBUG = True
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
